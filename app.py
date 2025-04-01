b324ffdc122fe36a6f576885e2ce49d22aa24b66
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-04-01 19:59:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,9 +80,9 @@ with st.sidebar:
     
     st.markdown("## ⚙️ Model Settings")
     model_options = [
-        "gemini-1.5-flash",
-        "gemini-1.5-pro",
-        "gemini-1.0-pro","gemini-2.0-flash"
+"gemini-2.0-flash", "gemini-1.5-flash", "gemini-1.5-pro","gemini-2.0-flash-lite","gemini-2.0-pro-exp-02-05",
+"gemini-2.0-flash-thinking-exp-01-21","gemini-2.5-pro-exp-03-25","gemini-1.5-flash-8b"],
+        
     ]
     
     selected_model = st.selectbox("Select Model:", model_options)
