c6bfdade1eb5c9e3e97be3fce918b9a486885863
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-04-06 15:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,12 +80,13 @@ with st.sidebar:
         with model_col1:
             model_option = st.selectbox(
                 "Model:", 
-                ["gemini-1.5-pro", "gemini-1.5-flash", "gemini-2.0-flash"]
+                ["gemini-2.0-flash","gemini-2.0-flash-lite","gemini-2.0-pro-exp-02-05",
+"gemini-2.0-flash-thinking-exp-01-21","gemini-1.5-flash-8b", "gemini-1.5-flash", "gemini-1.5-pro"]
             )
         
         with model_col2:
             st.markdown("<br>", unsafe_allow_html=True)
-            info_tooltip = st.info("i")
+            info_tooltip = st.info("")
             if info_tooltip:
                 st.info("""
                 
