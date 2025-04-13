7a0143e989470ae4fea20ac4b92516543bd08ee0
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-04-13 20:41:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pandas as pd
 st.set_page_config(page_title="Gemini", layout="wide", page_icon="💎")
 
 # Mode selection
-app_mode = st.radio("Choose Mode:", ["Chat", "File Upload"])
+app_mode = st.radio("Choose Mode:", ["Chat", "File Upload(Beta)"])
 
 if app_mode == "Chat":
     # Gradient-styled title using HTML
